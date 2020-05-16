e85e0a610703ec59ed5f3bbf6255a04f8fe75ee1
user <user@MacBook-Pro-de-user.local>
2020-05-16 18:13:51 +0200

    deleted double dispatch

--- TestComponent.js
+++ TestComponent.js
@@ -1,36 +1,34 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+    import React, { useEffect } from 'react';
+    import { StyleSheet, Text, View } from 'react-native';
 
-import { useSelector, useDispatch } from 'react-redux';
-import * as testActions from './store/actions/test';
+    import { useSelector, useDispatch } from 'react-redux';
+    import * as testActions from './store/actions/test';
 
-const TestComponent = props => {
-  const dispatch = useDispatch();
+    const TestComponent = props => {
+      const dispatch = useDispatch();
 
-  var test = useSelector(state => state.test.test);
-  console.log('test='+test);
+      var test = useSelector(state => state.test.test);
+      console.log('test='+test);
 
-  dispatch(testActions.fetchTest());
+      useEffect(() => {
+          
+          dispatch(testActions.fetchTest());
+        }, [dispatch]);
 
-  useEffect(() => {
-      
-      dispatch(testActions.fetchTest());
-    }, [dispatch]);
+        return (
+        <View style={styles.container}>
+          <Text> Test is {test} </Text>
+        </View>
+        );
+    };
 
-    return (
-    <View style={styles.container}>
-      <Text> Test is {test} </Text>
-    </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }
-  });
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }
+      });
 
 
-export default TestComponent;
+    export default TestComponent;
